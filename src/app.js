23ace16f296d0854b1966e8a1047ef23ccdd96cf
padejar <thdarkman61@gmail.com>
2023-06-11 21:47:16 +0700

    .

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const { default: puppeteer } = require('puppeteer');
 const moment = require('moment/moment');
+const { isValidURL, waitForNetworkIdle } = require('./utils');
 
 const index = (_, res) => {
     return res.render(__dirname + '/templates/index.ejs');
@@ -20,6 +21,7 @@ const urlChecker = async (url) => {
     try {
         const browser = await puppeteer.launch({
             args: ['--no-sandbox'],
+            headless: 'new',
         });
         const page = await browser.newPage();
         await page.goto(url);
@@ -39,43 +41,6 @@ const urlChecker = async (url) => {
         console.error(e);
     }
 } 
-
-const waitForNetworkIdle = async (page, timeout = 0, maxInflightRequest = 0) => {
-    page.on('request', onRequestStarted);
-    page.on('requestfinished', onRequestFinished);
-    page.on('requestfailed', onRequestFinished);
-
-    let inflight = 0;
-    let fulfill;
-    const promise = new Promise((x) => (fulfill = x));
-    let timeoutId = setTimeout(onTimeoutDone, timeout);
-    return promise;
-
-    function onTimeoutDone() {
-        page.removeListener('request', onRequestStarted);
-        page.removeListener('requestfinished', onRequestFinished);
-        page.removeListener('requestfailed', onRequestFinished);
-        fulfill();
-    }
-
-    function onRequestStarted() {
-        ++inflight;
-        if (inflight > maxInflightRequests) clearTimeout(timeoutId);
-    }
-
-    function onRequestFinished() {
-        if (inflight === 0) return;
-        --inflight;
-        if (inflight === maxInflightRequests) timeoutId = setTimeout(onTimeoutDone, timeout);
-    }
-}
-
-function isValidURL(url) {
-    const pattern = new RegExp(
-      /^(https?:\/\/)?([a-z\d]+([a-z\d-]*[a-z\d])*\.)+[a-z]{2,}(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i
-    );
-    return pattern.test(url);
-}
 
 module.exports = function(app) {
     app.get('/', index);
